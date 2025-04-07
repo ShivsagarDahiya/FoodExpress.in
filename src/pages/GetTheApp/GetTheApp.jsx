dd89f657f08cef9513d78ea342c40362465dd302
shivsagar Dahiya <shivsagardahayat99@gmail.com>
2025-04-08 00:07:16 +0530

    Initial commit

--- src/pages/GetTheApp/GetTheApp.jsx
+++ src/pages/GetTheApp/GetTheApp.jsx
@@ -1,17 +1,19 @@
-import css from './GetTheApp.module.css'
+import css from "./GetTheApp.module.css";
 
-import Navbar2 from '../../components/Navbars/NavigationBar2/NavigationBar2'
-import GetTheAppComp from '../../components/HomeComponents/GetTheApp/GetTheApp'
-import Footer from '../../components/Footer/Footer'
+import Navbar2 from "../../components/Navbars/NavigationBar2/NavigationBar2";
+import GetTheAppComp from "../../components/HomeComponents/GetTheApp/GetTheApp";
+import Footer from "../../components/Footer/Footer";
 
 const GetTheApp = () => {
-  return <div className={css.outerDiv}>
-    <div className={css.innerDiv}>
+  return (
+    <div className={css.outerDiv}>
+      <div className={css.innerDiv}>
         <Navbar2 />
         <GetTheAppComp />
         <Footer />
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-export default GetTheApp
+export default GetTheApp;
